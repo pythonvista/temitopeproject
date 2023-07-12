13943928405e460d95864f8ab2a2ce214c99c769
pythonvista <emeterv18@gmail.com>
2023-07-12 14:20:02 +0100

    upatev

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,16 @@
 <template>
-  <div>
-    home
+  <div class="h-screen flex justify-center items-center flex-col">
+    <p class="text-3xl font-bold">Temitope Project (matric no)</p>
+    <p class="text-3xl font-bold">(University)</p>
+    <p class="text-3xl font-bold">(Department)</p>
+    <q-btn :to="{ path: '/login' }">Student Login</q-btn>
+    <q-btn :to="{ path: '/register' }">Register Student</q-btn>
+     <q-btn :to="{ path: '/admin' }">Admin</q-btn>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
